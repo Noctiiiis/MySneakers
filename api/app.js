d29ddb5aeacd25ae134ff922ebab53420d5c5415
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-04 15:02:22 +0100

    Handle errors

--- api/app.js
+++ api/app.js
@@ -5,4 +5,19 @@ const productsRoute = require('./routes/products');
 
 app.use('/products', productsRoute);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
 module.exports = app;
