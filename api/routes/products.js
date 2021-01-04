57d0a3d84a37ec51a4d0984e56d0d687f10c230a
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-04 16:55:13 +0100

    Have the route to get a specific product

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,12 +3,27 @@ const router = express.Router();
 
 const Product = require('../models/Product');
 
-// Gets the path /products from app.js
+// Gets the path /products from app.js which gets all the products
 router.get('/', (req, res, next) => {
     Product.find()
         .exec()
         .then(documents => {
             res.status(200).json(documents)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({error: error})
+        });
+});
+
+// Gets the product corresponding to the id given in the url
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    Product.findById(id)
+        .exec()
+        .then(document => {
+            res.status(200).json(document)
         })
         .catch(error => {
             console.log(error);
