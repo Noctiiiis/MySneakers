b3a67abb8defd16226b074ae376ebd8572f7e6b6
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-04 16:34:01 +0100

    Connect to the local database

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const productsRoute = require('./routes/products');
 
 app.use('/products', productsRoute);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mysneakers', {useNewUrlParser: true});
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
