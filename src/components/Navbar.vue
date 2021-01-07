0f32ccf72c651f46d6fb9de3ea97cd732f3d3edb
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-07 09:57:02 +0100

    Have a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <mdb-navbar color="white" light>
+    <mdb-navbar-brand>
+      <a href="/"><h1 id="brand">MySneakers</h1></a>
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <mdb-nav-item href="/products">Produits</mdb-nav-item>
+        <mdb-nav-item href="/products/men">Homme</mdb-nav-item>
+        <mdb-nav-item href="/products/women">Femme</mdb-nav-item>
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+</template>
+
+<script>
+import {mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem} from 'mdbvue'
+
+export default {
+  name: 'Navbar',
+  components: {
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavbarNav,
+    mdbNavItem
+  }
+}
+</script>
+
+<style>
+  #brand {
+    color: black;
+    font-weight: bold;
+    font-style: italic;
+  }
+</style>
