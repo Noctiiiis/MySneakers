92082f1640f8414243fe40b506e1304f1ac5f18b
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-07 13:38:44 +0100

    Allow CORS access

--- api/app.js
+++ api/app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const productsRoute = require('./routes/products');
 
+// Database connection
+mongoose.connect('mongodb://127.0.0.1:27017/mysneakers', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Cors access
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+})
+
+// Routes
 app.use('/products', productsRoute);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mysneakers', {useNewUrlParser: true});
-
+// Errors
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
