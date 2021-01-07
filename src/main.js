5157e156183fac921a2a216979e5059edc0ca6a1
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-07 13:52:56 +0100

    Include vue-router with some routes and vue-resource

--- src/main.js
+++ src/main.js
@@ -2,10 +2,27 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// Components
 import App from './App.vue'
+import Home from './components/Home.vue'
+import Products from './components/Products.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home }, 
+    { path: '/products', component: Products }, 
+  ]
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
