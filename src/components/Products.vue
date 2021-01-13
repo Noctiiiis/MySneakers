7a02112d7212fcafa18a63710a6f18a14c181928
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-13 17:07:45 +0100

    Update products UI

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,77 +1,45 @@
 <template>
-  <mdb-container fluid>
-    <h1 class="mt-xl-3 text-left" left>Products ({{ this.quantity }})</h1>
+  <mdb-container fluid class="px-5">
+    <header class="pt-3">
+      <h3>Products ({{ this.quantity }})</h3>
+    </header>
     <mdb-row center>
-      <div
-        class="m-xl-3 m-lg-3 m-md-2 m-sm-2 m-2"
-        v-for="product in products"
-        :key="product.id"
-      >
-        <mdb-card>
-          <mdb-view hover>
-            <router-link to="`/products/${ product.id }`">
-              <mdb-card-image :src="require(`../assets/${product.image}`)" />
-              <mdb-mask flex-center waves overlay="grey-slight"></mdb-mask>
-            </router-link>
-          </mdb-view>
-          <mdb-card-body>
-            <mdb-card-title>{{ product.name }}</mdb-card-title>
-            <mdb-card-text>CHF {{ product.price }}.-</mdb-card-text>
-            <mdb-btn color="grey darken-1">BUY</mdb-btn>
-          </mdb-card-body>
-        </mdb-card>
+      <div v-for="product in products" :key="product.id" class="m-2">
+        <product-card :product="product"></product-card>
       </div>
     </mdb-row>
   </mdb-container>
 </template>
 
 <script>
-import {
-  mdbContainer,
-  mdbRow,
-  mdbCard,
-  mdbCardImage,
-  mdbCardBody,
-  mdbCardTitle,
-  mdbCardText,
-  mdbBtn,
-  mdbView,
-  mdbMask,
-} from "mdbvue";
+import { mdbContainer, mdbRow } from "mdbvue";
+
+import ProductCard from "./ProductCard";
 
 export default {
+  name: "Products",
   components: {
     mdbContainer,
     mdbRow,
-    mdbCard,
-    mdbCardImage,
-    mdbCardBody,
-    mdbCardTitle,
-    mdbCardText,
-    mdbBtn,
-    mdbView,
-    mdbMask,
+    ProductCard,
   },
   mounted() {
+    ProductCard;
     this.$http.get("http://localhost:3000/products").then((response) => {
       response.json().then((data) => {
         this.products = data;
-        this.quantity = data.length
+        this.quantity = data.length;
       });
     });
   },
   data() {
     return {
       products: [],
-      quantity: 0
+      quantity: 0,
     };
   },
 };
 </script>
 
 <style>
-.card {
-  height: 30rem;
-  width: 23rem;
-}
 </style>
