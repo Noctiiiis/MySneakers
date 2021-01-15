8f960c1cfb19d1f7078906e4920cff46d6eeda1e
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-15 14:37:26 +0100

    Display the login page

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <mdb-container fluid>
+    <mdb-row center>
+      <mdb-col class="z-depth-1 col-lg-5 col-md-8 p-5 mx-5">
+        <h3 class="mb-5">Login</h3>
+        <mdb-input type="email" label="E-mail" />
+        <mdb-input type="password" label="Password" />
+        <mdb-btn color="grey darken-1">Login</mdb-btn>
+      </mdb-col>
+    </mdb-row>
+  </mdb-container>
+</template>
+
+<script>
+import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbBtn } from "mdbvue";
+
+export default {
+  name: "Login",
+  components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    mdbInput,
+    mdbBtn,
+  },
+};
+</script>
+
+<style>
+</style>
