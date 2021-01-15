e1dc5184ebd4827b77ebd6ac89142322a8fe9555
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-15 14:38:50 +0100

    Have the route for the login

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import App from './App'
 import Home from './components/Home'
 import Products from './components/Products'
 import ProductDetails from './components/ProductDetails'
+import Login from './components/Login'
 import NotFound from './components/NotFound'
 
 Vue.config.productionTip = false
@@ -24,6 +25,7 @@ const router = new VueRouter({
     { path: '/products/men', component: Products, props: { filter: 'men' } },
     { path: '/products/women', component: Products, props: { filter: 'women' } },
     { path: '/products/:id', component: ProductDetails, props: true },
+    { path: '/login', component: Login },
     { path: '/error', component: NotFound },
     { path: '*', component: NotFound }
   ]
