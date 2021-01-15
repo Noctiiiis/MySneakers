3a9456ad9a0113c8cf504fa20991f56744d9dad0
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-15 16:15:00 +0100

    Have the user routes

---
+++ api/routes/user.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User')
+
+router.post('/register', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Mail exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (error, hash) => {
+                    if (error) {
+                        return res.status(500).json({
+                            error: error
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(error => {
+                                res.status(500).json({
+                                    error: error
+                                })
+                            });
+                    }
+                })
+            }
+        })
+        .catch();
+});
+
+module.exports = router;
