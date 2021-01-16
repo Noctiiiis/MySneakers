23cbf08cfe27642755a075442a8b3f7c15dabb2c
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-16 16:27:37 +0100

    Have the order model

---
+++ api/models/Order.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    shoeSize: { type: String, required: true },
+    quantity: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
