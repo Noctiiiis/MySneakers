a22ab57eab5a67809b73da4302dc2c437d023a96
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-18 16:12:59 +0100

    Have a logout message

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <mdb-alert color="success" v-if="logout">
+      You have been successfully disconnected
+    </mdb-alert>
+  </div>
+</template>
+
+<script>
+import { mdbAlert } from "mdbvue";
+export default {
+  name: "Home",
+  components: {
+    mdbAlert,
+  },
+  props: {
+    logout: {
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
