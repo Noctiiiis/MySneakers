0000fbde30b368fe88a3fd9189f10b25b1fe7ab2
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-18 21:30:51 +0100

    Add user email to the order model

--- api/models/Order.js
+++ api/models/Order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    userEmail: { type: String, required: true },
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     shoeSize: { type: String, required: true },
     quantity: { type: Number, required: true }
