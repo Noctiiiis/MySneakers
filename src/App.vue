43beaf66adfcc5c4de8568a6c78e5deed25dd9fd
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-19 12:26:17 +0100

    Have a footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <navbar></navbar>
     <router-view></router-view>
+    <footerPage></footerPage>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import FooterPage from "./components/Footer";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    FooterPage,
   },
 };
 </script>
