963396701b226731bcab01f0f1839c2ce73ea477
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-21 09:44:36 +0100

    Have the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <mdb-container>
     <mdb-alert color="success" v-if="logout">
       You have been successfully disconnected
     </mdb-alert>
-  </div>
+    <mdb-row center>
+      <mdb-col class="col-6">
+        <h3>About us</h3>
+        <p>
+          MySneakers is a branded footwear sales company located in Sainte-Croix
+          (VD), Switzerland.
+        </p>
+        <p>
+          Our company appears as the leader of the Swiss market. We are
+          particularly well known for our high quality service as well as our
+          collection products.
+        </p>
+      </mdb-col>
+    </mdb-row>
+  </mdb-container>
 </template>
 
 <script>
-import { mdbAlert } from "mdbvue";
+import { mdbContainer, mdbRow, mdbCol, mdbAlert } from "mdbvue";
 export default {
   name: "Home",
   components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol,
     mdbAlert,
   },
   props: {
